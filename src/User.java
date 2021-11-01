bb4632fe4f667557da61008e9eeb1ec90a50b1d2
Valetdinov <arslan21372@gmail.com>
2021-11-01 11:27:33 +0500

    Добавление вывода

--- src/User.java
+++ src/User.java
@@ -37,13 +37,16 @@ public class User {
 
     public static void main(String[] args) {
       User u1 = new User((short) 25, "Алексей", 182);
-      System.out.println(u1.age +", " + u1.name +", " + u1.height);
+      System.out.println(u1.name +", " + u1.age +", " + u1.height);
       User u2 = new User((short) 18, 183, "Алеся");
-        System.out.println(u2.name +", "+ u2.age +", " + u2.height);
+        System.out.println(u2.age +", "+ u2.name +", " + u2.height);
       User u3 = new User((short) 29, "Олег", 177);
-        System.out.println(u3.name + ", " + u3.height + ", " + u3.age);
+        System.out.println(u3.height + ", " + u3.age + ", " + u3.name);
       User u4 = new User((short) 35, "Виталий" , 190);
-        System.out.println(u4.height + ", " + u4.name + ", " + u4.age);
-
+        System.out.println(u4.name + ", " + u4.height + ", " + u4.age);
+      User u5 = new User((short) 19, "Алина", 192);
+        System.out.println(u5.age + ", " + u5.height + ", " + u5.name);
+      User u6 = new User(182, "Марина", (short) 35);
+        System.out.println(u6.height + ", " + u6.name + ", " + u6.age);
     }
 }
